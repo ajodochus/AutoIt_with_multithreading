61e8004c09094fe6759d21766c6a8c6710507eb2
unknown <wissmeier@googlemail.com>
2015-12-15 21:59:51 +0100

    second thread is watching for notepad and closes it if found. Main thread is navigating through db.de homepage

--- src/NewThread.java
+++ src/NewThread.java
@@ -1,6 +1,7 @@
 class NewThread implements Runnable {
    Thread t;
    boolean shouldStop = false;
+   int count = 1;
    NewThread() {
       // Create a new, second thread
       t = new Thread(this, "Demo Thread");
@@ -11,9 +12,11 @@ class NewThread implements Runnable {
    // This is the entry point for the second thread.
    public void run() {
       try {
-    	  while(!shouldStop) {
-    		  CalcTest calcTest = new CalcTest();
-          }
+    	 // while(!shouldStop) {
+    		  CalcTest2 calcTest2 = new CalcTest2();
+    		  count++;
+    		  System.out.print("count: " + count);
+         // }
          
      } catch (InterruptedException e) {
          System.out.println("Calc test couldnt be instanciated.");
